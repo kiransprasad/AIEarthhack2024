4143158e63baab7da0e4300521c1b4b0e587e53d
Cindy Li <rin.mp4@gmail.com>
2024-01-07 18:00:49 -0500

    nav stuff

---
+++ frontend/src/SearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Table from "./components/Table/Table";
+
+
+
+function Home() {
+    return (
+        <h1> This is a search page </h1>
+    );
+}
+
+export default Home;
