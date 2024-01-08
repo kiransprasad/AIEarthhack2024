438d2d1d83a7bb0759b5f56b9473a182a5085df8
Kiran Prasad <kiranprsd@Kirans-MacBook-Pro.local>
2024-01-07 20:13:03 -0500

    Final merge?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,17 +9,14 @@ import './index.css';
 function App() {
   return (
     <Router>
-    <Navbar data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/search">Search</Nav.Link>
-            <Nav.Link href="/view-all">View All</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar bsStyle="inverse" collapseOnSelect>
+      <Navbar.Collapse>
+        <Nav>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/search">Search</Nav.Link>
+          <Nav.Link href="/view-all">View All</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
 
       <Routes>
